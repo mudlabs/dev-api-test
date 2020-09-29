f03308ad70ed39a0dc223ad1416f95ff2c50ab23
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-09-30 08:14:25 +1000

    Update index.js

--- .github/actions/dev/index.js
+++ .github/actions/dev/index.js
@@ -1,15 +1,7 @@
-// const unirest = require("unirest");
-const fetch = require("node-fetch");
+const unirest = require("unirest");
 
 (async function(){
   try {
-   const post = await fetch(
-     "https://dev.to/api/articles",
-     headers: { 
-      'Content-Type': "application/json", 
-     'api-key': process.env.dev_to_token 
-     },
-     body: JSON.stringify()
    const post = await unirest
      .post("https://dev.to/api/articles")
       .headers({
@@ -19,10 +11,10 @@ const fetch = require("node-fetch");
       .send(JSON.stringify({
         "title": "Hello, World!",
         "published": false,
-      "body_markdown": "Hello DEV, this is my first post",
-       "tags": ["discuss", "help"],
+        "body_markdown": "Hello DEV, this is my first post",
+        "tags": ["discuss", "help"],
         "series": "Hello series",
-      }));
+       }));
      console.log(post.body);
    } catch(error) {
      console.log(error);
