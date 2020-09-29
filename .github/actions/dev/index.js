86d9d998c4d9095ded28c645daf4908a5aaaccd3
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-09-30 08:19:34 +1000

    Update index.js

--- .github/actions/dev/index.js
+++ .github/actions/dev/index.js
@@ -1,20 +1,39 @@
 const unirest = require("unirest");
+const fetch = require("node-fetch");
 
 (async function(){
   try {
-   const post = await unirest
-     .post("https://dev.to/api/articles")
-      .headers({
-       'Content-Type': "application/json",
-       'api-key': process.env.token
+   const post = await fetch(
+     "https://dev.to/api/articles",
+     {
+       method: "post",
+       headers: { 
+         'Content-Type': "application/json",
+         'api-key': process.env.token
+       },
+       body: JSON.stringify({
+          "title": "Hello, World!",
+          "published": false,
+          "body_markdown": "Hello DEV, this is my first post",
+          "tags": ["discuss", "help"],
+          "series": "Hello series"
        })
-      .send(JSON.stringify({
-        "title": "Hello, World!",
-        "published": false,
-        "body_markdown": "Hello DEV, this is my first post",
-        "tags": ["discuss", "help"],
-        "series": "Hello series",
-       }));
+     }
+   );
+   
+//    const post = await unirest
+//      .post("https://dev.to/api/articles")
+//       .headers({
+//        'Content-Type': "application/json",
+//        'api-key': process.env.token
+//        })
+//       .send(JSON.stringify({
+//         "title": "Hello, World!",
+//         "published": false,
+//         "body_markdown": "Hello DEV, this is my first post",
+//         "tags": ["discuss", "help"],
+//         "series": "Hello series"
+//        }));
      console.log(post.body);
    } catch(error) {
      console.log(error);
