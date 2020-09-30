44913871b508bccacd04dc8b161aa948fba823d5
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-09-30 15:24:57 +1000

    Update index.js

--- .github/actions/dev/index.js
+++ .github/actions/dev/index.js
@@ -4,38 +4,38 @@ const fetch = require("node-fetch");
 
 (async function(){
   try {
-    const axiosPost = await axios({
-      method: "post",
-      url: "https://dev.to/api/articles",
-      headers: { 
-        'Content-Type': "application/json",
-        'api-key': process.env.token 
-      },
-      data: {
-        article: {
-          title: "Hello Dev 1",
-          published: false,
-          body_markdown: "Helloe Dev.to, this is my first post.",
-          tags: ["api", "post"],
-          series: "API"
-        }
-      }
-    });
-    console.log(axiosPost);
+//     const axiosPost = await axios({
+//       method: "post",
+//       url: "https://dev.to/api/articles",
+//       headers: { 
+//         'Content-Type': "application/json",
+//         'api-key': process.env.token 
+//       },
+//       data: {
+//         article: {
+//           title: "Hello Dev 1",
+//           published: false,
+//           body_markdown: "Helloe Dev.to, this is my first post.",
+//           tags: ["api", "post"],
+//           series: "API"
+//         }
+//       }
+//     });
+//     console.log(axiosPost);
     
-    const unirestPost = await unirest
-      .post("https://dev.to/api/articles")
-      .headers({ 'Content-Type': "application/json", 'api-key': process.env.token })
-      .send({
-        article:{
-          title: "Hello Dev 2",
-          published: false,
-          body_markdown: "Hello Dev.to, this is my second post.",
-          tags: ["api", "post"],
-          series: "API"
-        }
-      });
-    console.log(unirestPost);
+//     const unirestPost = await unirest
+//       .post("https://dev.to/api/articles")
+//       .headers({ 'Content-Type': "application/json", 'api-key': process.env.token })
+//       .send({
+//         article:{
+//           title: "Hello Dev 2",
+//           published: false,
+//           body_markdown: "Hello Dev.to, this is my second post.",
+//           tags: ["api", "post"],
+//           series: "API"
+//         }
+//       });
+//     console.log(unirestPost);
     
     const fetchPost = await fetch(
       "https://dev.to/api/articles",
