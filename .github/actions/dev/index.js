91f5c172da960e6d18e525dc6bbc394898ed6bd1
Sam Donald <32623552+mudlabs@users.noreply.github.com>
2020-09-30 15:15:17 +1000

    Update index.js

--- .github/actions/dev/index.js
+++ .github/actions/dev/index.js
@@ -1,27 +1,28 @@
-const axios = require("axios");
+// const axios = require("axios");
 const unirest = require("unirest");
 const fetch = require("node-fetch");
 
 (async function(){
   try {
-    const axiosPost = await axios({
-      method: "post",
-      url: "https://dev.to/api/articles",
-      headers: { 
-        'Content-Type': "application/json",
-        'api-key': process.env.token 
-      },
-      data: {
-        article: {
-          title: "Hello Dev 1",
-          published: false,
-          body_markdown: "Helloe Dev.to, this is my first post.",
-          tags: ["api", "post"],
-          series: "API"
-        }
-      }
-    });
-    console.log(axiosPost);
+//     const axiosPost = await axios({
+//       method: "post",
+//       url: "https://dev.to/api/articles",
+//       headers: { 
+//         'Content-Type': "application/json",
+//         'api-key': process.env.token 
+//       },
+//       data: {
+//         article: {
+//           title: "Hello Dev 1",
+//           published: false,
+//           body_markdown: "Helloe Dev.to, this is my first post.",
+//           tags: ["api", "post"],
+//           series: "API"
+//         }
+//       }
+//     });
+//     console.log(axiosPost);
+    
     const unirestPost = await unirest
       .post("https://dev.to/api/articles")
       .headers({ 'Content-Type': "application/json", 'api-key': process.env.token })
